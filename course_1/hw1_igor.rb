c6789173ab4913215fbd668cacda82c8b59e60a0
bokalo <gist03@ukr.net>
2015-07-27 12:09:05 +0300

    Update hw1_igor.rb

--- course_1/hw1_igor.rb
+++ course_1/hw1_igor.rb
@@ -1,94 +1,53 @@
-my_data = {f_name: 'Igor', l_name: 'Bokalo', age: '26', education: 'higher Education', hobby: 'like traveling and have fun',
-    awesome: 'this summer I want to jump with a parachute'}
+my_data = {f_name: 'Igor', l_name: 'Bokalo',
+	age: '26', education: 'higher education',
+	hobby: 'like traveling and have fun', 
+	awesome: 'this summer I want to jump with a parachute'}
 
-my_aims = [['1. Buy MacBook Air'],
-            ['2. Buy bike Ducati'],
-                ['3. Visit Italy, Paris, London, Washington and New York City'],
-                 ['4. Create my own family'],
-                    ['5. Buy a house']]
+my_aims = ["Buy MacBook Air", 
+			"Buy bike Ducati", 
+			"Visit Italy, Paris, London, Washington and New York City",
+			"Create my own family",
+			"Buy a house"]
 
-my_steps = [['Learn ROR and find a good job with a very good salary'],
-                ['Get a driver\'s license for motorcycle and to make some money savings'],
-                    ['Get cool company and take an international passport' ],
-                        ['Buy a ring with a big diamond and make a proposal to my girlfriend'],
-                            ['Make greate savings with my wife and sold a bike;(']]
+my_steps = ["Learn ROR and find a good job with a very good salary", 
+			"Get a driver\'s license for motorcycle and to make some money savings",
+			"Get cool company and take an international passport'",
+			"Buy a ring with a big diamond and make a proposal to my girlfriend",
+			"Make greate savings with my wife and sold a bike;("]
 
+
+opt = 0
 
 puts 'Hi, I\'m Aims Program. My creator is ' + my_data [:f_name]+ ' ' + my_data [:l_name] + '.'
 puts 'He is '+my_data[:age]+'. He has '+my_data[:education]+', he '+my_data[:hobby]+'.'
-
-    puts 'Here you can see all Igor\'s aims for five years.
-    If you want to see all aims, please press 0.
-    If you want to see the proper year, press from 1 to 5.
-    Make your choice:'
-
-    choice=gets.chomp.to_i
-
-        if choice==0
-            puts my_aims[0...5]
-        end
-
-
-    if choice==1
-        puts my_aims[0]
-        puts 'If you want to see steps for this aim, please press \'y\'.
-If you don\'t want to see steps for this aim press \'n\'.'
-    choice=gets
-            if choice == "y\n"
-                puts my_steps[0]
-            end
-            if choice =="n\n"
-                puts 'Good day!'
-            end
+until opt == 3
+  puts "******************************************"
+  puts "Please choose one of options in menu:\n 1 - my aims for all years\n 2 - my aims with steps for proper year\n 3 - exit"
+  opt = gets.to_i
+  if opt == 1
+    puts "My aims:"
+    puts my_aims
+  elsif opt == 2
+    sub_opt = 0
+    until sub_opt == 3
+      puts "********************************************************"
+      puts "Please choose year(1-5):"
+      year = gets.to_i
+      puts "My aims for #{year} year: #{my_aims[year-1]}"
+      puts "What you want to do next?\n 1- steps \n 2 - another year \n 3 - main menu"
+      sub_opt = gets.to_i
+      if sub_opt == 1
+        puts "My steps for #{year} year: #{my_steps[year-1]}"
+      elsif sub_opt == 2
+      elsif sub_opt == 3
+      else
+          puts "choose 1-3"
+      end
     end
 
-        if choice==2
-            puts my_aims[1]
-            puts 'If you want to see steps for this aim, please press \'y\'.
-If you don\'t want to see steps for this aim press \'n\'.'
-    choice=gets
-            if choice == "y\n"
-                puts my_steps[1]
-            end
-            if choice =="n\n"
-                puts 'Good day!'
-            end
-    end
-
-            if choice==3
-                puts my_aims[2]
-                puts 'If you want to see steps for this aim, please press \'y\'.
-If you don\'t want to see steps for this aim press \'n\'.'
-    choice=gets
-            if choice == "y\n"
-                puts my_steps[2]
-            end
-            if choice =="n\n"
-                puts 'Good day!'
-            end
-            end
-
-                if choice==4
-                    puts my_aims[3]
-                    puts 'If you want to see steps for this aim, please press \'y\'.
-If you don\'t want to see steps for this aim press \'n\'.'
-    choice=gets
-            if choice == "y\n"
-                puts my_steps[3]
-            end
-            if choice =="n\n"
-                puts 'Good day!'
-            end
-                end
-                    if choice==5
-                        puts my_aims[4]
-                        puts 'If you want to see steps for this aim, please press \'y\'.
-If you don\'t want to see steps for this aim press \'n\'.'
-    choice=gets
-            if choice == "y\n"
-                puts my_steps[4]
-            end
-            if choice =="n\n"
-                puts 'Good day!'
-            end
-                    end
+  elsif opt == 3
+    puts "Thanks for choose our Aim\'s Program! Have a nice day."
+  else
+    puts "Please choose 1-3 options."
+  end
+end
